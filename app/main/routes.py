e6fabe7cf9229829c11d918a5d62ad8061e84622
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-05-29 21:23:38 -0500

    feat: edit

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import render_template, request, flash, redirect, url_for, Response
+from flask import render_template, request, flash, redirect, url_for, Response, jsonify
 from flask_login import login_required, current_user
 
 from app import db
@@ -27,23 +27,37 @@ def work_experience():
     db.session.commit()
     if request.method == "POST":
         try:
-            candidate = Candidate.query.filter_by(user_id=current_user.id).first()
-            end_date = datetime.strptime(request.form["end_date"], "%Y-%m-%d") if request.form.get("end_date",
-                                                                                                   None) else None
+            if request.form['edit_id']:
+                candidate_work_experience = CandidateWorkExperience.query.filter_by(id=request.form['edit_id']).first()
+                candidate_work_experience.company_name = request.form['company_name']
+                candidate_work_experience.job_title = request.form['job_title']
+                candidate_work_experience.start_date = datetime.strptime(request.form['start_date'], "%Y-%m-%d")
+                candidate_work_experience.end_date = datetime.strptime(request.form['end_date'], "%Y-%m-%d") if \
+                    request.form['end_date'] else None
+                candidate_work_experience.currently_working = True if request.form.get('currently_working',
+                                                                                       '') == 'on' else False
+                db.session.commit()
+                flash(f'La experiencia laboral fue actualizada exitosamente!');
+            else:
 
-            candidate_work_experience = CandidateWorkExperience(company_name=request.form["company_name"],
-                                                                job_title=request.form["job_title"],
-                                                                start_date=datetime.strptime(request.form["start_date"],
-                                                                                             "%Y-%m-%d"),
-                                                                end_date=end_date,
-                                                                currently_working=True if request.form.get(
-                                                                    "currently_working", '') == 'on' else False,
-                                                                candidate_id=candidate.id)
+                candidate = Candidate.query.filter_by(user_id=current_user.id).first()
+                end_date = datetime.strptime(request.form["end_date"], "%Y-%m-%d") if request.form.get("end_date",
+                                                                                                       None) else None
 
-            db.session.add(candidate_work_experience)
-            db.session.commit()
+                candidate_work_experience = CandidateWorkExperience(company_name=request.form["company_name"],
+                                                                    job_title=request.form["job_title"],
+                                                                    start_date=datetime.strptime(
+                                                                        request.form["start_date"],
+                                                                        "%Y-%m-%d"),
+                                                                    end_date=end_date,
+                                                                    currently_working=True if request.form.get(
+                                                                        "currently_working", '') == 'on' else False,
+                                                                    candidate_id=candidate.id)
 
-            flash(f'La experiencia laboral fue actualizada exitosamente!')
+                db.session.add(candidate_work_experience)
+                db.session.commit()
+
+                flash(f'La experiencia laboral fue actualizada exitosamente!')
         except:
             flash(f'Hubo un problema actualizando la experiencia laboral, intente mas tarde...!')
 
@@ -67,27 +81,62 @@ def delete_work_experience():
         return Response(status=500);
 
 
+@bp.route("/get_work_experience", methods=["GET"])
+@login_required
+def get_work_experience():
+    try:
+        print(request.args['id'])
+        candidate_work_experience = CandidateWorkExperience.query.filter_by(id=request.args['id']).first()
+        new_dict = {
+            'id': candidate_work_experience.id,
+            'company_name': candidate_work_experience.company_name,
+            'job_title': candidate_work_experience.job_title,
+            'start_date': candidate_work_experience.start_date.strftime("%Y-%m-%d"),
+            'end_date': candidate_work_experience.end_date.strftime(
+                "%Y-%m-%d") if candidate_work_experience.end_date else '',
+            'currently_working': candidate_work_experience.currently_working
+        }
+        return jsonify(new_dict)
+
+    except:
+        return Response(status=500);
+
+
 @bp.route("/education", methods=["GET", "POST"])
 @login_required
 def education():
     db.session.commit()
     if request.method == "POST":
         try:
-            candidate = Candidate.query.filter_by(user_id=current_user.id).first()
-            yog = datetime.strptime(request.form["year_of_graduation"], "%Y-%m-%d") if request.form.get(
-                "year_of_graduation", None) else None
+            if request.form['edit_id']:
+                candidate_education = CandidateEducation.query.filter_by(id=request.form['edit_id']).first()
+                candidate_education.university = request.form['university']
+                candidate_education.degree_name = request.form['degree_name']
+                candidate_education.year_of_graduation = datetime.strptime(request.form["year_of_graduation"],
+                                                                           "%Y-%m-%d") if request.form.get(
+                    "year_of_graduation", None) else None
+                candidate_education.currently_enrolled = True if request.form.get("currently_enrolled",
+                                                                                  '') == 'on' else False
+                db.session.commit()
+                flash(f'La educación fue actualizada exitosamente!')
+            else:
 
-            candidate_education = CandidateEducation(university=request.form["university"],
-                                                     degree_name=request.form["degree_name"],
-                                                     year_of_graduation=yog,
-                                                     currently_enrolled=True if request.form.get("currently_enrolled",
-                                                                                                 '') == 'on' else False,
-                                                     candidate_id=candidate.id)
+                candidate = Candidate.query.filter_by(user_id=current_user.id).first()
+                yog = datetime.strptime(request.form["year_of_graduation"], "%Y-%m-%d") if request.form.get(
+                    "year_of_graduation", None) else None
 
-            db.session.add(candidate_education)
-            db.session.commit()
+                candidate_education = CandidateEducation(university=request.form["university"],
+                                                         degree_name=request.form["degree_name"],
+                                                         year_of_graduation=yog,
+                                                         currently_enrolled=True if request.form.get(
+                                                             "currently_enrolled",
+                                                             '') == 'on' else False,
+                                                         candidate_id=candidate.id)
 
-            flash(f'La experiencia académica fue actualizada exitosamente!')
+                db.session.add(candidate_education)
+                db.session.commit()
+
+                flash(f'La experiencia académica fue actualizada exitosamente!')
         except:
             flash(f'Hubo un problema actualizando la experiencia académica, intente mas tarde...!')
 
@@ -107,6 +156,24 @@ def delete_education():
         return Response(status=200);
     except:
         flash(f'Hubo un problema eliminando la experiencia académica, intente mas tarde...!');
+        return Response(status=500);
+
+
+@bp.route("/get_education", methods=["GET"])
+@login_required
+def get_education():
+    try:
+        candidate_education = CandidateEducation.query.filter_by(id=request.form['id']).first()
+        education = {
+            'id': candidate_education.id,
+            'university': candidate_education.university,
+            'degree_name': candidate_education.degree_name,
+            'year_of_graduation': candidate_education.year_of_graduation.strftime(
+                "%Y-%m-%d") if candidate_education.year_of_graduation else None,
+            'currently_enrolled': candidate_education.currently_enrolled
+        }
+        return jsonify(education)
+    except:
         return Response(status=500);
 
 
